035d728a70173a3e646f353761f097444cfeeb23
Fernando Abolafio <fernandohsabolafio@gmail.com>
2022-06-16 14:52:09 +0200

    fix error

--- nextjs/src/pages/api/poetry/new.ts
+++ nextjs/src/pages/api/poetry/new.ts
@@ -6,13 +6,20 @@ import { getUserFromSession } from "../../../session";
 
 export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Poetry>
+  res: NextApiResponse<Poetry | { error: string }>
 ) {
   const { title, content } = JSON.parse(req.body);
 
   console.log("oi");
 
   const user = await getUserFromSession({ req, res });
+
+  if (!user) {
+    res
+      .status(401)
+      .json({ error: "You must be logged in to create a new poetry" });
+    return;
+  }
 
   const poetry = await db.poetry.create({
     data: {
